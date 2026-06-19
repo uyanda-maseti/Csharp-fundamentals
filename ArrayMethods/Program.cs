52c78eecf449c24428e5fa0a1db58db55af17665
Uyanda <masetibabalo2@gmail.com>
2026-06-19 15:53:42 +0200

    practise using the clear array method

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] pallets = [ "B14", "A11", "B12", "A13" ];
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
