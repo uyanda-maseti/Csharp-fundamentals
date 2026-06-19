f3d2fc9b4b3f7c0d4e0c9c5933e8c824692e7315
Uyanda <masetibabalo2@gmail.com>
2026-06-19 19:54:01 +0200

    reverse a string

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -1,36 +1,46 @@
-﻿string[] pallets = [ "B14", "A11", "B12", "A13" ];
-Console.WriteLine("");
+﻿// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+// Console.WriteLine("");
 
-Console.WriteLine($"Before {pallets[0].ToLower()} and {pallets[1].ToLower()}");
-Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Before {pallets[0].ToLower()} and {pallets[1].ToLower()}");
+// Array.Clear(pallets, 0, 2);
 
-if (pallets[0] != null)
-    Console.WriteLine($"After---{pallets[0].ToLower()}---and---{pallets[1].ToLower()}");
+// if (pallets[0] != null)
+//     Console.WriteLine($"After---{pallets[0].ToLower()}---and---{pallets[1].ToLower()}");
 
-// Console.WriteLine();
-// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// // Console.WriteLine();
+// // Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// // foreach (var pallet in pallets)
+// // {
+// //     Console.WriteLine($"-- {pallet}");
+// // }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
 // foreach (var pallet in pallets)
 // {
 //     Console.WriteLine($"-- {pallet}");
 // }
 
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
 
-pallets[4] = "C01";
-pallets[5] = "C02";
+// foreach(var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
 
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
 
-Console.WriteLine("");
-Array.Resize(ref pallets, 3);
-Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Console.WriteLine(valueArray);
+Array.Reverse(valueArray);
+string result = new string(valueArray);
+Console.WriteLine(result);
 
-foreach(var pallet in pallets)
-{
-    Console.WriteLine($"--{pallet}");
-}
+
