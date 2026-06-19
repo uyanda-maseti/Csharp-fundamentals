f6ec5ee6595a7d4c4db0cf7daa1cbbb86b7816b1
Uyanda <masetibabalo2@gmail.com>
2026-06-19 16:25:49 +0200

    examine the the arguments of the clear() method

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("");
 
 Console.WriteLine($"Before {pallets[0]} and {pallets[1]}");
-Array.Clear(pallets, 0, 2);
+Array.Clear(pallets, 2, 2);
 Console.WriteLine($"After---{pallets[0]}---and---{pallets[1]}");
 
 Console.WriteLine();
