d64ebac8ae35ff344c61cbc43da3a9f4b0f7ba02
Uyanda <masetibabalo2@gmail.com>
2026-06-19 16:48:12 +0200

    try using the Resize() array method and add two new elements to the array

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -7,8 +7,20 @@ Array.Clear(pallets, 0, 2);
 if (pallets[0] != null)
     Console.WriteLine($"After---{pallets[0].ToLower()}---and---{pallets[1].ToLower()}");
 
-Console.WriteLine();
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// Console.WriteLine();
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
--- ArrayMethods/obj/Debug/net10.0/ArrayMethods.AssemblyInfo.cs
+++ ArrayMethods/obj/Debug/net10.0/ArrayMethods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb7045271081c84d587ccc518639584e8267c3c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e63b82f33ebbf85566fe4f9dc97d77b1c648469")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
