7e63b82f33ebbf85566fe4f9dc97d77b1c648469
Uyanda <masetibabalo2@gmail.com>
2026-06-19 16:37:13 +0200

    fix the error by validating if the value is null

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -1,9 +1,11 @@
 ﻿string[] pallets = [ "B14", "A11", "B12", "A13" ];
 Console.WriteLine("");
 
-Console.WriteLine($"Before {pallets[0]} and {pallets[1]}");
-Array.Clear(pallets, 1, 3);
-Console.WriteLine($"After---{pallets[0]}---and---{pallets[1]}");
+Console.WriteLine($"Before {pallets[0].ToLower()} and {pallets[1].ToLower()}");
+Array.Clear(pallets, 0, 2);
+
+if (pallets[0] != null)
+    Console.WriteLine($"After---{pallets[0].ToLower()}---and---{pallets[1].ToLower()}");
 
 Console.WriteLine();
 Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
--- ArrayMethods/obj/Debug/net10.0/ArrayMethods.AssemblyInfo.cs
+++ ArrayMethods/obj/Debug/net10.0/ArrayMethods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ec5ee6595a7d4c4db0cf7daa1cbbb86b7816b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb7045271081c84d587ccc518639584e8267c3c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
