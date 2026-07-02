c8a4b0a23383bd4ab9a2c649db7fb1dc4033d656
Uyanda <masetibabalo2@gmail.com>
2026-07-03 01:21:00 +0200

    finish the first line

--- stringFormat/Program.cs
+++ stringFormat/Program.cs
@@ -39,15 +39,43 @@
 // Console.WriteLine(input.PadLeft(12,'-'));
 // Console.WriteLine(input.PadRight(12,'-'));
 
-string paymentId = "769C";
-string payeeName = "Mr. Stephen Ortega";
-string paymentAmount = "$5,000.00";
+// string paymentId = "769C";
+// string payeeName = "Mr. Stephen Ortega";
+// string paymentAmount = "$5,000.00";
 
-var formattedLine = paymentId.PadRight(6);
-formattedLine += payeeName.PadRight(24);
-formattedLine += paymentAmount.PadLeft(10);
+// var formattedLine = paymentId.PadRight(6);
+// formattedLine += payeeName.PadRight(24);
+// formattedLine += paymentAmount.PadLeft(10);
 
-Console.WriteLine("1234567890123456789012345678901234567890");
-Console.WriteLine(formattedLine);
+// Console.WriteLine("1234567890123456789012345678901234567890");
+// Console.WriteLine(formattedLine);
+
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+string currentReturnCom = $"{currentReturn:P2}";
+string currentProfitCom = $"{currentProfit:C}";
+comparisonMessage += currentProduct.PadRight(20);
+comparisonMessage += currentReturnCom.PadRight(10,'-');
+comparisonMessage += currentProfitCom;
+
+Console.WriteLine(comparisonMessage);
+
 
 Console.WriteLine();
