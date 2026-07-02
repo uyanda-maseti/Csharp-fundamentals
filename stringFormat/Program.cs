94c2150b7a18d8c1e588dffd5da9b8cf6dded05b
Uyanda <masetibabalo2@gmail.com>
2026-07-03 01:46:19 +0200

    well at least now i know i shoul have used the string format method

--- stringFormat/Program.cs
+++ stringFormat/Program.cs
@@ -58,22 +58,30 @@ int currentShares = 2975000;
 decimal currentReturn = 0.1275m;
 decimal currentProfit = 55000000.0m;
 
-// string newProduct = "Glorious Future";
-// decimal newReturn = 0.13125m;
-// decimal newProfit = 63000000.0m;
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
 
 Console.WriteLine($"Dear {customerName},");
 Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
 Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}.\n");
 
 Console.WriteLine("Here's a quick comparison:\n");
 
 string comparisonMessage = "";
 string currentReturnCom = $"{currentReturn:P2}";
 string currentProfitCom = $"{currentProfit:C}";
+string newReturnCom = $"{newReturn:P2}";
+string newProfitCom = $"{newProfit:C}";
 comparisonMessage += currentProduct.PadRight(20);
-comparisonMessage += currentReturnCom.PadRight(10,'-');
-comparisonMessage += currentProfitCom;
+comparisonMessage += currentReturnCom.PadRight(10);
+comparisonMessage += $"{currentProfitCom}\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += newReturnCom.PadRight(10);
+comparisonMessage += newProfitCom;
+
+
 
 Console.WriteLine(comparisonMessage);
 
--- stringFormat/obj/Debug/net10.0/stringFormat.AssemblyInfo.cs
+++ stringFormat/obj/Debug/net10.0/stringFormat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stringFormat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48eb674fc152c962fee5d586d9e3c2f4762f14f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a4b0a23383bd4ab9a2c649db7fb1dc4033d656")]
 [assembly: System.Reflection.AssemblyProductAttribute("stringFormat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stringFormat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
