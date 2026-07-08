8a276ef845a5961e5a70dd93f3e0d4fec30be2ce
Uyanda <masetibabalo2@gmail.com>
2026-07-08 13:52:52 +0200

    draw a shape

--- Objects/Program.cs
+++ Objects/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("          ^");
+Console.WriteLine("        /    \\" );
+Console.WriteLine("       /      \\");
+Console.WriteLine("      /        \\");
+Console.WriteLine("     /__________\\");
